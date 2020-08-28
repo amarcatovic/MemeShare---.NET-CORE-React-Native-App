062b6ddc9e48ef83b4bf33034895b5519b036dbb
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:40:25 +0200

    Add MemeTypeRead Dto

---
+++ MemeSharingAPI/Data/Dtos/MemeTypeReadDto.cs
@@ -0,0 +1,14 @@
+﻿using MemeSharingAPI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Dtos
+{
+    public class MemeTypeReadDto
+    {
+        public string TypeName { get; set; }
+        public ICollection<Meme> Memes { get; set; }
+    }
+}
