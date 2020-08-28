3c00bb31fa52c1ee22e94102d950cc6fb87d2e6d
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:08:24 +0200

    Add Migrations

---
+++ MemeSharingAPI/Migrations/20200828110815_InitialBuild.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemeSharingAPI.Migrations
+{
+    public partial class InitialBuild : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MemeTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MemeTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    PublicId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: false),
+                    Likes = table.Column<long>(nullable: false, defaultValue: 0L),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    PhotoId = table.Column<int>(nullable: false),
+                    MemeTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Memes_MemeTypes_MemeTypeId",
+                        column: x => x.MemeTypeId,
+                        principalTable: "MemeTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Memes_Photos_PhotoId",
+                        column: x => x.PhotoId,
+                        principalTable: "Photos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memes_MemeTypeId",
+                table: "Memes",
+                column: "MemeTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memes_PhotoId",
+                table: "Memes",
+                column: "PhotoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Memes");
+
+            migrationBuilder.DropTable(
+                name: "MemeTypes");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
