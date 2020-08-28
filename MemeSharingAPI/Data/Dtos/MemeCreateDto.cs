d93770b5c8306ca4bd01d1a36624b5372a1c1ba6
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 14:26:08 +0200

    Add Dto for meme creation

---
+++ MemeSharingAPI/Data/Dtos/MemeCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Dtos
+{
+    public class MemeCreateDto
+    {
+        public MemeCreateDto()
+        {
+            DateAdded = DateTime.Now;
+        }
+
+        public string Title { get; set; }
+        public long Likes { get; set; }
+        public DateTime DateAdded { get; set; }
+        public IFormFile PhotoFile { get; set; }
+        public int MemeTypeId { get; set; }
+    }
+}
