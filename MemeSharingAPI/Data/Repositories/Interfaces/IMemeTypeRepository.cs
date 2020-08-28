e37c5db4891a493da5515ab26c41f63e6dc7fb8e
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:18:18 +0200

    Add IMemeTypeRepo

---
+++ MemeSharingAPI/Data/Repositories/Interfaces/IMemeTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using MemeSharingAPI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Repositories.Interfaces
+{
+    public interface IMemeTypeRepository
+    {
+        Task<MemeType> GetMemeTypeById(int id);
+        Task<IEnumerable<MemeType>> GetAllTypes();
+    }
+}
