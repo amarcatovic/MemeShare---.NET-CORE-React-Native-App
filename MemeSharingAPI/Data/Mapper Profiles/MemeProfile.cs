e26dfe2f0516bd64a1905a5170f503e1340450a1
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 15:48:25 +0200

    Create Meme AutoMapper Dto

---
+++ MemeSharingAPI/Data/Mapper Profiles/MemeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MemeSharingAPI.Data.Dtos;
+using MemeSharingAPI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Mapper_Profiles
+{
+    public class MemeProfile : Profile
+    {
+        public MemeProfile()
+        {
+            CreateMap<Meme, MemeReadDto>();
+        }
+    }
+}
