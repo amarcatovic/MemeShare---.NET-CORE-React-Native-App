0e8ec31dee1b220c060cb7679eec924e9c1e0369
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 15:46:31 +0200

    Create Meme and Photo Read Dto

---
+++ MemeSharingAPI/Data/Dtos/MemeReadDto.cs
@@ -0,0 +1,17 @@
+﻿using MemeSharingAPI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Dtos
+{
+    public class MemeReadDto
+    {
+        public string Title { get; set; }
+        public long Likes { get; set; }
+        public DateTime DateAdded { get; set; }
+        public PhotoReadDto Photo { get; set; }
+        public MemeTypeReadDto MemeType { get; set; }
+    }
+}
---
+++ MemeSharingAPI/Data/Dtos/PhotoReadDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Dtos
+{
+    public class PhotoReadDto
+    {
+        public string Url { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+    }
+}
