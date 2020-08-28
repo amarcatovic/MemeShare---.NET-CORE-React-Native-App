f913d281fc5ac95c53533f7aaa79735ee607875f
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:03:31 +0200

    Add DbContext Class

---
+++ MemeSharingAPI/Data/MemesContext.cs
@@ -0,0 +1,29 @@
+﻿using MemeSharingAPI.Data.Entity_Configurations;
+using MemeSharingAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data
+{
+    public class MemesContext : DbContext
+    {
+        public MemesContext(DbContextOptions<MemesContext> opt) : base (opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new MemeConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Meme> Memes { get; set; }
+        public DbSet<MemeType> MemeTypes { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
