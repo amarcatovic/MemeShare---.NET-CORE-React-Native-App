df1955d18177b71dd60219c7568cbab65358b0e4
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:35:48 +0200

    Add MemeTypes Controller

---
+++ MemeSharingAPI/Controllers/MemeTypesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MemeSharingAPI.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemeSharingAPI.Controllers
+{
+    [Route("api/types")]
+    [ApiController]
+    public class MemeTypesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMemeTypeRepository _repo;
+
+        public MemeTypesController(IMapper mapper, IMemeTypeRepository repo)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+    }
+}
