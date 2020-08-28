040db90c51d2460b2ce60e215637eef5890ba231
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 12:49:03 +0200

    Model Photo Class

--- MemeSharingAPI/Data/Models/Photo.cs
+++ MemeSharingAPI/Data/Models/Photo.cs
@@ -7,5 +7,11 @@ namespace MemeSharingAPI.Data.Models
 {
     public class Photo
     {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+
+        public ICollection<Meme> Memes { get; set; }
     }
 }
