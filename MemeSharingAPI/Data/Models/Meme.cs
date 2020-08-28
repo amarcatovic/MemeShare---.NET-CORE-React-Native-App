4d7ae5713e0d8657986a0fe93b0c486d6accabb9
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 12:47:31 +0200

    Create Meme Model Class

---
+++ MemeSharingAPI/Data/Models/Meme.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Models
+{
+    public class Meme
+    {
+    }
+}
