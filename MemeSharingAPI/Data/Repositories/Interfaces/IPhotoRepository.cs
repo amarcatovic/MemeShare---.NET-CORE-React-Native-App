3bc5198a804bd466adf43170e2edd0953d778931
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:19:50 +0200

    Add IPhotoRepo

---
+++ MemeSharingAPI/Data/Repositories/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,13 @@
+﻿using MemeSharingAPI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Repositories.Interfaces
+{
+    public interface IPhotoRepository
+    {
+        Task<Photo> GetPhotoById(int id);
+    }
+}
