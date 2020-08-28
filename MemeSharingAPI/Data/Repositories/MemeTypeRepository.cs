31be0eb8ac7cf360cd2fd46d03240c7dde4a9fa1
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:27:46 +0200

    Implement IMemeTypeRepo

---
+++ MemeSharingAPI/Data/Repositories/MemeTypeRepository.cs
@@ -0,0 +1,30 @@
+﻿using MemeSharingAPI.Data.Models;
+using MemeSharingAPI.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Repositories
+{
+    public class MemeTypeRepository : IMemeTypeRepository
+    {
+        private readonly MemesContext _context;
+
+        public MemeTypeRepository(MemesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MemeType>> GetAllTypes()
+        {
+            return await _context.MemeTypes.ToListAsync();
+        }
+
+        public async Task<MemeType> GetMemeTypeById(int id)
+        {
+            return await _context.MemeTypes.FindAsync(id);
+        }
+    }
+}
