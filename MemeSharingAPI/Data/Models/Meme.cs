b4eab5a2128d50caff35c37a9582ea3cec0b779a
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 12:55:51 +0200

    Model Meme Class

--- MemeSharingAPI/Data/Models/Meme.cs
+++ MemeSharingAPI/Data/Models/Meme.cs
@@ -7,5 +7,12 @@ namespace MemeSharingAPI.Data.Models
 {
     public class Meme
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public long Likes { get; set; }
+        public Photo Photo { get; set; }
+        public int PhotoId { get; set; }
+        public MemeType MemeType { get; set; }
+        public int MemeTypeId { get; set; }
     }
 }
