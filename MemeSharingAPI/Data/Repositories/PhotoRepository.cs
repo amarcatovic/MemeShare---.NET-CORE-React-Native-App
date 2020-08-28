c08e08f23b96ea9ca034906469b77d5ba81c2dc9
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-28 13:28:50 +0200

    Implement IPhotoRepo

---
+++ MemeSharingAPI/Data/Repositories/PhotoRepository.cs
@@ -0,0 +1,23 @@
+﻿using MemeSharingAPI.Data.Models;
+using MemeSharingAPI.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeSharingAPI.Data.Repositories
+{
+    public class PhotoRepository : IPhotoRepository
+    {
+        private readonly MemesContext _context;
+
+        public PhotoRepository(MemesContext context)
+        {
+            _context = context;
+        }
+        public async Task<Photo> GetPhotoById(int id)
+        {
+            return await _context.Photos.FindAsync(id);
+        }
+    }
+}
